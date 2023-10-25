7dda2adcf0cfdbddf668e9aa216ea296c2f9190d
Z4 <30985576+TheProdigyLeague@users.noreply.github.com>
2023-10-25 06:18:08 -0500

    Update node js   app --push.bash

--- .github/workflows/node js   app --push.bash
+++ .github/workflows/node js   app --push.bash
@@ -15,7 +15,9 @@ _description: Point users to a newly created issue and closes the original issue
 _template:
   name: node-app
   repo: node-express
+
 # learn akam(cTM)
+
 on:
     push:
             branches:
@@ -47,8 +49,41 @@ jobs:
         app-name: ${{ env.AZURE_WEBAPP_NAME }}
         publish-profile: ${{ secrets.AZURE_WEBAPP_PUBLISH_PROFILE }}
         package: ${{ env.AZURE_WEBAPP_PACKAGE_PATH }}
+
 $ runtime: nodejs10
-<pre id="output" class="d-inline-block text-left mb-0" style="overflow-y: hidden;"> /$$      /$$ /$$           /$$                           /$$                 <br>| $$$    /$$$|__/          |__/                          |__/                 <br>| $$$$  /$$$$ /$$ /$$$$$$$  /$$ /$$$$$$$   /$$$$$$        /$$ /$$$$$$$        <br>| $$ $$/$$ $$| $$| $$__  $$| $$| $$__  $$ /$$__  $$      | $$| $$__  $$       <br>| $$  $$$| $$| $$| $$  \ $$| $$| $$  \ $$| $$  \ $$      | $$| $$  \ $$       <br>| $$\  $ | $$| $$| $$  | $$| $$| $$  | $$| $$  | $$      | $$| $$  | $$       <br>| $$ \/  | $$| $$| $$  | $$| $$| $$  | $$|  $$$$$$$      | $$| $$  | $$       <br>|__/     |__/|__/|__/  |__/|__/|__/  |__/ \____  $$      |__/|__/  |__/       <br>                                          /$$  \ $$                           <br>                                         |  $$$$$$/                           <br>                                          \______/                            <br> /$$$$$$$                                                                     <br>| $$__  $$                                                                    <br>| $$  \ $$ /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$$<br>| $$$$$$$//$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$_____//$$_____/<br>| $$____/| $$  \__/| $$  \ $$| $$  \ $$| $$  \__/| $$$$$$$$|  $$$$$$|  $$$$$$ <br>| $$     | $$      | $$  | $$| $$  | $$| $$      | $$_____/ \____  $$\____  $$<br>| $$     | $$      |  $$$$$$/|  $$$$$$$| $$      |  $$$$$$$ /$$$$$$$//$$$$$$$/<br>|__/     |__/       \______/  \____  $$|__/       \_______/|_______/|_______/ <br>                              /$$  \ $$                                       <br>                             |  $$$$$$/                                       <br>                              \______/                                        <br>                                                                              <br>                                                                              <br>                                                                              <br>                                                                              <br>                                                                              <br>                                                                              <br> /$$ /$$ /$$                                                                  <br>|__/|__/|__/                                                                  </pre>
+
+<pre id="output" class="d-inline-block text-left mb-0" style="overflow-y: hidden;"> 
+/$$      /$$ /$$           /$$                           /$$                 
+<br>| $$$    /$$$|__/          |__/                          |__/                 
+<br>| $$$$  /$$$$ /$$ /$$$$$$$  /$$ /$$$$$$$   /$$$$$$        /$$ /$$$$$$$       
+<br>| $$ $$/$$ $$| $$| $$__  $$| $$| $$__  $$ /$$__  $$      | $$| $$__  $$       
+<br>| $$  $$$| $$| $$| $$  \ $$| $$| $$  \ $$| $$  \ $$      | $$| $$  \ $$       
+<br>| $$\  $ | $$| $$| $$  | $$| $$| $$  | $$| $$  | $$      | $$| $$  | $$       
+<br>| $$ \/  | $$| $$| $$  | $$| $$| $$  | $$|  $$$$$$$      | $$| $$  | $$       
+<br>|__/     |__/|__/|__/  |__/|__/|__/  |__/ \____  $$      |__/|__/  |__/       
+<br>                                          /$$  \ $$                           
+<br>                                         |  $$$$$$/                           
+<br>                                          \______/                            
+<br> /$$$$$$$                                                                     
+<br>| $$__  $$                                                                    
+<br>| $$  \ $$ /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$$
+<br>| $$$$$$$//$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$_____//$$_____/
+<br>| $$____/| $$  \__/| $$  \ $$| $$  \ $$| $$  \__/| $$$$$$$$|  $$$$$$|  $$$$$$ 
+<br>| $$     | $$      | $$  | $$| $$  | $$| $$      | $$_____/ \____  $$\____  $$\
+<br>| $$     | $$      |  $$$$$$/|  $$$$$$$| $$      |  $$$$$$$ /$$$$$$$//$$$$$$$/
+<br>|__/     |__/       \______/  \____  $$|__/       \_______/|_______/|_______/ 
+<br>                              /$$  \ $$                                       
+<br>                             |  $$$$$$/                                       
+<br>                              \______/                                        
+<br>                                                                              
+<br>                                                                              
+<br>                                                                              
+<br>                                                                              
+<br>                                                                             
+<br>                                                                              
+<br> /$$ /$$ /$$                                                                 
+<br>|__/|__/|__/                                                                  
+</pre>
 $ sql 
 ~# [PUSH]
     name: var_sub .json, .xml, '&&' .yml -fs
@@ -65,3 +100,48 @@ $ sql
                 block: "crypto"
                 SECRET: ${{ secrets.BLOCK_CHAIN}}
 $ _ |
+name: Deploy Node.js to Azure Web App
+
+on:
+  [push]
+
+# CONFIGURATION
+# For help, go to https://github.com/Azure/Actions
+#
+# 1. Set up the following secrets in your repository:
+#   AZURE_WEBAPP_PUBLISH_PROFILE
+#
+# 2. Change these variables for your configuration:
+env:
+  AZURE_WEBAPP_NAME: your-app-name    # set this to your application's name
+  AZURE_WEBAPP_PACKAGE_PATH: '.'      # set this to the path to your web app project, defaults to the repository root
+  NODE_VERSION: '16.x'                # set this to the node version to use
+
+jobs:
+  build-and-deploy:
+    name: Build and Deploy
+    runs-on: ubuntu-latest
+    environment: dev
+    steps:
+    - uses: actions/checkout@v3
+    - name: Use Node.js ${{ env.NODE_VERSION }}
+      uses: actions/setup-node@v3
+      with:
+        node-version: ${{ env.NODE_VERSION }}
+    - name: npm install, build, and test
+      run: |
+        # Build and test the project, then
+        # deploy to Azure Web App.
+        npm prune --production
+        npm install
+        npm run build --if-present
+        # npm run test --if-present
+    - name: 'Deploy to Azure WebApp'
+      uses: azure/webapps-deploy@v2
+      with: 
+        app-name: ${{ env.AZURE_WEBAPP_NAME }}
+        publish-profile: ${{ secrets.AZURE_WEBAPP_PUBLISH_PROFILE }}
+        package: ${{ env.AZURE_WEBAPP_PACKAGE_PATH }}
+        
+  # For more information on GitHub Actions for Azure, refer to https://github.com/Azure/Actions
+  # For more samples to get started with GitHub Action workflows to deploy to Azure, refer to https://github.com/Azure/actions-workflow-samples
